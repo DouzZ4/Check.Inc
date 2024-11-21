fdf96ec654e164ab6a55dd1735220f8ddaaa425b
DouzZ4 <angeleduardomen@gmail.com>
2024-11-21 15:34:22 -0500

    man tan bueno yo ome

---
+++ Check/src/assets/Scripts/Main.js
@@ -0,0 +1,11 @@
+// main.js
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menuToggle");
+    const navLinks = document.getElementById("navLinks");
+
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener("click", () => {
+            navLinks.classList.toggle("show");
+        });
+    }
+});
