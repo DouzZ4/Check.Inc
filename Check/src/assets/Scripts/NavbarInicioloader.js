8a903724293b2f91b0195079ee17a6aee0f88ed5
DouzZ4 <angeleduardomen@gmail.com>
2024-11-21 11:41:13 -0500

    valiendo verga

--- Check/src/assets/Scripts/NavbarInicioloader.js
+++
@@ -1,6 +0,0 @@
-fetch('../Components/NavbarPagInicio.html')
-    .then(response => response.text())
-    .then(data => {
-        document.querySelector('#navbar-container').innerHTML = data;
-    })
-    .catch(error => console.error('Error:', error));
