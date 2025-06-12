ac7fb5f757e13fec31a7b5c3c15e07bd1341bfbf
Angel Mendez <angeleduardomen@gmail.com>
2025-06-11 22:16:58 -0500

    pagina de login cargada

---
+++ src/main/java/com/mycompany/checkinc/controller/login.java
@@ -0,0 +1,25 @@
+package com.mycompany.checkinc.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author angel
+ */
+@Named(value = "login")
+@Dependent
+public class login {
+
+    /**
+     * Creates a new instance of login
+     */
+    public login() {
+    }
+    
+}
