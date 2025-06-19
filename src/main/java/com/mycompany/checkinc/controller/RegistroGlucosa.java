814e2befe179c454676f24250b6be01860ab5fc5
Angel Mendez <angeleduardomen@gmail.com>
2025-06-19 00:09:39 -0500

    pagina de registro glucosa funcionando y mostrando datos

--- src/main/java/com/mycompany/checkinc/controller/RegistroGlucosa.java
+++ src/main/java/com/mycompany/checkinc/controller/RegistroGlucosa.java
@@ -13,6 +13,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.line.LineChartModel;
+import org.primefaces.model.charts.line.LineChartDataSet;
 
 @ManagedBean(name = "registroGlucosa")
 @ViewScoped
@@ -31,6 +34,7 @@ public class RegistroGlucosa implements Serializable {
     private Date fechaHora;
     private List<Glucosa> registros;
     private boolean editando;
+    private LineChartModel lineChartModel;
     
     public RegistroGlucosa() {
         this.fechaHora = new Date();
@@ -42,10 +46,32 @@ public class RegistroGlucosa implements Serializable {
         if (usuario != null) {
             cargarRegistros(usuario);
         }
+        initLineChart();
     }
     
     private void cargarRegistros(Usuario usuario) {
         this.registros = glucosaFacade.findByUsuario(usuario);
+        initLineChart(); // Actualiza el gráfico al cargar registros
+    }
+
+    private void initLineChart() {
+        lineChartModel = new LineChartModel();
+        ChartData data = new ChartData();
+        LineChartDataSet dataSet = new LineChartDataSet();
+        java.util.List<Object> values = new java.util.ArrayList<>();
+        java.util.List<String> labels = new java.util.ArrayList<>();
+        if (registros != null) {
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm");
+            for (Glucosa g : registros) {
+                values.add(g.getNivelGlucosa());
+                labels.add(sdf.format(g.getFechaHora()));
+            }
+        }
+        dataSet.setData(values);
+        dataSet.setLabel("Nivel de Glucosa");
+        data.setLabels(labels);
+        data.addChartDataSet(dataSet);
+        lineChartModel.setData(data);
     }
     
     // Getters y Setters
@@ -63,6 +89,15 @@ public class RegistroGlucosa implements Serializable {
     public boolean isEditando() { return editando; }
     public void setEditando(boolean editando) { this.editando = editando; }
 
+    public LineChartModel getLineChartModel() {
+        return lineChartModel;
+    }
+
+    // Método para el botón 'Ahora' en la vista
+    public void setFechaHoraNow() {
+        this.fechaHora = new Date();
+    }
+    
     // Métodos de acción
     public void registrar() {
         Usuario usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(USUARIO_SESSION_KEY);
@@ -92,6 +127,7 @@ public class RegistroGlucosa implements Serializable {
             }
             cargarRegistros(usuario);
             limpiarFormulario();
+            initLineChart(); // Actualiza el gráfico después de registrar
         } catch (Exception e) {
             addMessage(FacesMessage.SEVERITY_ERROR, ERROR, "No se pudo procesar el registro: " + e.getMessage());
         }
