7f555834124a5bd1335d5ff1c16a9fd3a6522ca7
asmc1806 <a-cmoreno@hotmail.com>
2025-09-03 17:05:37 -0500

    dataTables_Aplicados

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,9 +1,42 @@
-$(document).ready(function() {
-    $('#usuariosForm\\:usuario').DataTable({
-        language: {
-            url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
-        },
-        pageLength: 10,
-        responsive: true
-    });
+$(document).ready(function () {
+    if ($('#usuariosForm\\:usuario').length) {
+        $('#usuariosForm\\:usuario').DataTable({
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
+            },
+            pageLength: 10,
+            responsive: true
+        });
+    }
+
+    if ($('#tablaForm\\:cita').length) {
+        $('#tablaForm\\:cita').DataTable({
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
+            },
+            pageLength: 10,
+            responsive: true
+        });
+    }
+    
+    if ($('#tablaForm\\:medicamento').length) {
+        $('#tablaForm\\:medicamento').DataTable({
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
+            },
+            pageLength: 10,
+            responsive: true
+        });
+    }
+    
+    if ($('#tablaForm\\:registro').length) {
+        $('#tablaForm\\:registro').DataTable({
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
+            },
+            pageLength: 10,
+            responsive: true
+        });
+    }
+    
 });
